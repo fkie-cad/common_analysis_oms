80fcccc77892e93da662bc303f9fdf7b8bb4fd3c
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2016-03-29 14:27:06 +0200

    plugins and tests added

--- tests/test_oms.py
+++ tests/test_oms.py
@@ -4,18 +4,41 @@ Created on Mar 24, 2016
 @author: weidenba
 '''
 import unittest
+from os import path
+
+from common_analysis_oms.oms import CommonAnalysisOMS
+
+BASE_DIR = path.dirname(path.abspath(__file__))
+BENIGN_FILE_PATH = path.join(BASE_DIR, "data/benign")
+MALICIOUS_FILE_PATH = path.join(BASE_DIR, "data/eicar")
 
 
 class Test(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.oms = CommonAnalysisOMS()
 
     def tearDown(self):
         pass
 
-    def testName(self):
-        pass
+    def test_plugin_init(self):
+        self.assertGreater(len(self.oms.av_list), 0, "no scanners installed, please install at least clamav")
+
+    def test_get_av_scan_result(self):
+        self.assertEqual(self.oms.get_av_scan_result({"command": "echo $filepath"}, "test"), "test\n")
+
+    def test_find_malware_name(self):
+        self.assertEqual(self.oms.find_malware_name("test string", {"re_malware_name": "str([\w]+)"}), "ing")
+
+    def test_scan_benign(self):
+        result = self.oms.scan_file(BENIGN_FILE_PATH)
+        self.assertEqual(result["positives"], 0)
+        self.assertTrue(True not in [result["scans"][av]["detected"] for av in result["scans"]])
+
+    def test_scan_malicious(self):
+        result = self.oms.scan_file(MALICIOUS_FILE_PATH)
+        self.assertEqual(result["positives"], result['number_of_scanners'])
+        self.assertTrue(False not in [result["scans"][av]["detected"] for av in result["scans"]])
 
 
 if __name__ == "__main__":
